940768bb8e9175f776c6650f0a5692249031ca66
Rohan7585 <rohan178.rd@gmail.com>
2025-05-10 22:52:53 +0530

    Adding Doubly Linked List

---
+++ Linked List/DoublyLLFromScratch/DoublyLinkedList.java
@@ -0,0 +1,76 @@
+package DoublyLLFromScratch;
+
+public class DoublyLinkedList {
+    Node head;
+
+/*=======================
+Traverse The Linked List
+========================*/
+    public void display(){
+
+    }
+    
+
+/*=======================
+Insert value in the Linked List
+========================*/
+    public void add(int data){
+
+    }
+
+    public void InsertAtTheBeginning(int data){
+
+    }
+
+    public void InsertAtGivenPosition(int data, int position){
+
+    }
+
+
+/*=======================
+Delete Node from the Linked List
+========================*/
+    public void DeleteFirstNode(){
+
+    }
+
+    public void DeleteLastNode(){
+
+    }
+
+    public void DeleteGivenNode(int position){
+
+    }
+
+    public void DeleteByValue(int value){
+
+    }
+
+
+/*=======================
+Search Value from the Linked List
+========================*/
+    public int SearchByValue(int value){
+        return 0;
+    }
+
+    public int SearchByPosition(int position){
+        return 0;
+    }
+
+
+/*=======================
+Update Value of a node
+========================*/
+    public void UpdateValue(int position, int data){
+    
+    }
+
+
+/*=======================
+Count the nodes (length)
+========================*/
+    public int CountLength(){
+        return 0;
+    }
+}
