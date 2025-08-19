4a3fbf86df4f8f32fcca3d899cdaccdb2010cd37
Rohan7585 <rohan178.rd@gmail.com>
2025-08-19 20:39:44 +0530

    Adding Queue

---
+++ Queue/ImplementationUsingLL.java/QueueNode.java
@@ -0,0 +1,10 @@
+package Queue.ImplementationUsingLL;
+public class QueueNode {
+    int data;
+    QueueNode node;
+
+    public QueueNode(int data){
+        this.data = data;
+        node =  null;
+    }
+}
---
+++ Queue/ImplementationUsingLL.java/queue.java
@@ -0,0 +1,6 @@
+package Queue.ImplementationUsingLL;
+public class queue {
+    private int front=-1, tail=-1, len = 0;
+    private String emptyStr= "The queue is empty";
+    private QueueNode head;
+}
